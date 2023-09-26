dfbe72c79cdad57ad0a2fea37771ecb2450ab8db
Atte Kivimaki <aj.kivimaki@gmail.com>
2023-09-26 20:25:14 +0300

    add high score

--- main.js
+++ main.js
@@ -6,53 +6,54 @@ const scoreDisplay = document.querySelector(".score");
 const modalDisplay = document.querySelector("#overlay");
 const closeModalElem = document.querySelector("#close-overlay");
 const gameResultElem = document.querySelector("#game-result");
+const highScoreDisplay = document.querySelector("#high-score");
 
 /* - - - GLOBAL VARIABLES - - - */
 let score = 0;
 let timer = 0;
-let pace = 800;
+let pace = 1000;
 let current = 0;
 let rounds = 0;
+let highScore = parseInt(localStorage.getItem("highScore")) || 0;
 
-// let highScore = JSON.parse(localStorage.getItem("highScore")) || 0;
+// display high score
+highScoreDisplay.textContent = highScore;
 
 /* - - - FUNCTIONS - - - */
 const playGame = () => {
-  startButtonElem.classList.add("hide");
-  endButtonElem.classList.remove("hide");
-
   if (rounds >= 3) return endGame();
+  rounds++;
+
   enableEvents();
+  switchButtons();
 
   const newActiveNumber = picNewNumber(current);
 
-  circleElems[newActiveNumber].classList.toggle("active");
-  circleElems[current].classList.remove("active");
-  // generateRandomMushroom(newActiveNumber);
+  generateRandomMushroom(newActiveNumber, current);
 
   current = newActiveNumber;
   timer = setTimeout(playGame, pace);
-  rounds++;
+
   pace -= 10;
 
   function picNewNumber(current) {
     const newActiveNumber = getRandomNumber(0, 3);
-    return newActiveNumber !== current
-      ? newActiveNumber
-      : picNewNumber(current);
+    return newActiveNumber === current
+      ? picNewNumber(current)
+      : newActiveNumber;
   }
 };
 
 const endGame = () => {
   clearTimeout(timer);
   gameResult(score);
-  modalDisplay.classList.remove("hide");
+  showModal();
 };
 
 const clickCircle = (index) => {
   if (index !== current) return endGame();
   rounds--;
-  updateScore();
+  if (index === current && index === 3) score++;
   renderScore();
 };
 
@@ -62,23 +63,42 @@ const enableEvents = () => {
   });
 };
 
+const showModal = () => modalDisplay.classList.remove("hide");
 const resetGame = () => location.reload();
-const updateScore = () => score++;
 const renderScore = () => (scoreDisplay.textContent = score);
+const getRandomNumber = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+const switchButtons = () => {
+  startButtonElem.classList.add("hide");
+  endButtonElem.classList.remove("hide");
+};
+
 const gameResult = (score) => {
   let result = "";
-  if (score < 5) {
-    result = `You are slow! You have only ${score} mushrooms.`;
-  } else if (score > 10) {
-    result = `You are quite quick! You have ${score} mushrooms.`;
-  } else {
+  if (score === 0) {
+    result = "What's wrong with you? You didn't get any mushrooms!";
+  } else if (score > 0 && score < 5) {
+    result = `You are so slow! You got only ${score} mushrooms.`;
+  } else if (score >= 5 && score < 10) {
     result = `Not bad, ${score} mushrooms.`;
+  } else {
+    result = `You are quite quick! You got ${score} mushrooms.`;
   }
+  saveHighScore(score);
   gameResultElem.textContent = result;
 };
 
-const getRandomNumber = (min, max) =>
-  Math.floor(Math.random() * (max - min + 1)) + min;
+const generateRandomMushroom = (newActiveNumber, current) => {
+  circleElems[
+    newActiveNumber
+  ].innerHTML = `<img class="mushroom-img" src="pics/mushroom-${newActiveNumber}.png" alt="mushroom" />`;
+  circleElems[current].innerHTML = "";
+};
+
+const saveHighScore = (score) => {
+  if (score > highScore) localStorage.setItem("highScore", score);
+};
 
 /* - - - EVENT LISTENERS - - - */
 startButtonElem.addEventListener("click", playGame);
@@ -86,15 +106,4 @@ endButtonElem.addEventListener("click", endGame);
 closeModalElem.addEventListener("click", resetGame);
 circleElems.forEach((circle, index) => {
   circle.addEventListener("click", () => clickCircle(index));
-}); // pass the index of each circle into the clickCircle function
-
-/* const savehighScore = () => {
-  if (score > highScore) {
-    localStorage.setItem("highScore", JSON.stringify({ highScore: score }));
-  }
-}; */
-
-/* const generateRandomMushroom = (newActiveNumber) => {
-  const randomCircle = document.querySelector(`#circle${newActiveNumber}`);
-  randomCircle.innerHTML = `<img class="mushroom-img" src="pics/mushroom-${newActiveNumber}.png" alt="mushroom" />`;
-}; */
+});
